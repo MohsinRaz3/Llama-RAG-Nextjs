90140a4233398d97918712d1117be293e172f87a
mohsinraz3 <mohsinraz3@gmail.com>
2023-12-17 11:50:02 +0500

    favicon added

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -14,8 +14,8 @@ export default function ChatAvatar({ role }: { role: string }) {
     <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
       <Image
         className="rounded-md"
-        src="/llama.png"
-        alt="Llama Logo"
+        src="/panaverse.png"
+        alt="panaverse Logo"
         width={24}
         height={24}
         priority
